7d78308925b92afd03cee52ecf6161bb900657b3
Daniel Guerrero <ldguerrerop@hotmail.com>
2016-08-18 18:51:52 -0500

    Add serves static files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/client'))
+
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/client/index.html');
 });
@@ -9,13 +11,6 @@ app.get('/kardex', function(request, response){
     response.sendFile(__dirname + '/client/kardex.html');
 });
 
-app.get('/putameadre', function(request, response){
-    response.sendFile(__dirname + '/client/style.css');
-});
-
-app.get('/google', function(request, response){
-    response.sendFile(__dirname + 'https://www.google.com.mx/');
-});
 app.get('/404', function (request, response) {
   response.sendFile(__dirname + '/client/404.html');
 })
