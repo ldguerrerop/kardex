0602db0288909d3fcdc399957fd6359fbbe83e80
Daniel Guerrero <ldguerrerop@hotmail.com>
2016-08-18 13:26:06 -0500

    Show Kardex Daniel

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + '/client/index.html');
 });
 
+app.get('/kardex', function(request, response){
+    response.sendFile(__dirname + '/client/kardex.html');
+});
+
+app.get('/putameadre', function(request, response){
+    response.sendFile(__dirname + '/client/style.css');
+});
+
+app.get('/google', function(request, response){
+    response.sendFile(__dirname + 'https://www.google.com.mx/');
+});
 app.get('/404', function (request, response) {
   response.sendFile(__dirname + '/client/404.html');
 })
